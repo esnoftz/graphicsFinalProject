4eb36dc85911705efd4cf0da6f8c3eeaafc34132
esnoftz <esnoftz@gmail.com>
2024-02-13 22:43:55 -0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Creating the frame
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Creating the panel
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(236, 88, 0));
+
+        // Creating components
+        JLabel label1 = new JLabel("Welcome to Tiger Ball!");
+        JButton button1 = new JButton("START");
+
+        // Adding components to the panel
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.gridx = 1;
+        gc.gridy = 2;
+        panel.add(label1, gc);
+
+        gc.gridx = 2;
+        gc.gridy = 3;
+        panel.add(button1, gc);
+
+
+
+        // Adding the panel to the frame
+        frame.add(panel);
+
+        // Making the frame visible
+        frame.setVisible(true);
     }
 }

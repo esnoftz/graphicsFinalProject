3626b2cf3804faf98959e03c9a4dc967b0faf83d
esnoftz <esnoftz@gmail.com>
2024-02-20 11:25:50 -0600

    Added labels and buttons.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,21 +9,32 @@ public class Main {
     public static void main(String[] args) {
         // Idea: Have three present boxes on the screen and have the user pick one. Then inside will be a random number of balls. Could call it 100 balls
         // or something. The goal is to fill up the jar with 100 balls in as few tries as possible.
+        // To be evil, sometimes some balls will "miss" the jar when being put in. Start out with random colored balls falling
+        // and then turn them green when they are accepted into the jar. Ones that turn red will have "missed" or something
 
         // Creating the frame
         JFrame frame = new JFrame();
         frame.setSize(500, 500);
 
         // Creating the panel
-        MyPanel panel = new MyPanel();
-        MyPanel panel2 = new MyPanel();
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(2, 3));
 
         // Creating components
-        JButton button1 = new JButton("Click Me!");
-
+        JButton button1 = new JButton("#1");
+        JButton button2 = new JButton("#2");
+        JButton button3 = new JButton("#3");
+        JLabel label1 = new JLabel("#1");
+        JLabel label2 = new JLabel("#2");
+        JLabel label3 = new JLabel("#3");
 
         // Adding components to panel
         panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(label1);
+        panel.add(label2);
+        panel.add(label3);
 
         // Adding panel to frame
         frame.add(panel);
@@ -33,7 +45,7 @@ public class Main {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //frame.add(panel2);
+
             }
         });
 
